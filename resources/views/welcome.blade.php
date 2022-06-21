cce517bb67d7faba589aaf67c517fd582e2e256a
EPIPHANE <epiphaneniada@gmail.com>
2022-06-21 16:47:34 +0000

    modif page d'acc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,22 +34,9 @@
           
             <div class="d-grid gap-2 d-md-block">
               <a href="{{route('etudiant.create')}}">
-              <button class="btn btn-primary" type="button">Ajouter un abonné</button></a>
+              <button class="btn btn-primary" type="button">Ajouter un etudiant</button></a>
+              <p>salut</p>
               
-              <a href="{{ route('etudiant')}}">
-              <button class="btn btn-primary butv" type="button">liste des abonnées</button></a>
-            </div>
-      </form>
-        
-      </div>
-
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
